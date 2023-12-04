51b35b39ef8a937d2def2a9ff3bfe86b3765f12d
roman.aimaletdinov <aymaletdinov2012@gmail.com>
2023-12-04 15:50:23 +0000

    updated description

--- make_pr.sh
+++ make_pr.sh
@@ -4,11 +4,19 @@ getTargetBranch() {
     local current_branch
     current_branch=$(git rev-parse --abbrev-ref HEAD)
 
+    # Will change main to your target branch
     is_updated_from_main="$(
         git log --graph --decorate --simplify-by-decoration --oneline |
             grep -c "Merge branch 'main' into $current_branch"
     )"
 
+    # Will change main to your target branch
+    is_updated_from_main="$(
+        git log --graph --decorate --simplify-by-decoration --oneline |
+            grep -c "Merge branch 'origin/main' into $current_branch"
+    )"
+
+    # Will change target_branch from main to your target branch
     local target_branch
     if [ "$is_updated_from_main" != 0 ]; then
         target_branch="main"
@@ -19,6 +27,8 @@ getTargetBranch() {
     echo "$target_branch"
 }
 
+# Current branch is: JIRA-1916-fix-crash-on-main-screen
+# Will be converted to: JIRA-1916: Fix crash on main screen
 function getTitle() {
     local current_branch current_branch_number current_branch_text
 
@@ -32,6 +42,7 @@ function getTitle() {
             awk '{print toupper(substr($0,0,1))tolower(substr($0,2))}' # Uppercase first character
     )
 
+    # Will change `JIRA` to your real Jira project abbreviation (from the jira task link)
     echo "JIRA-$current_branch_number: $trimmed_current_branch_text"
 }
 
@@ -39,14 +50,29 @@ function getPrDescription() {
     local current_branch_number description
     current_branch_number="$(git rev-parse --abbrev-ref HEAD | awk -F '[^0-9]+' '{ print $2 }')"
 
+    # Will change `JIRA` to your real Jira project abbreviation (from the jira task link)
     description="
-### [$(getTitle)](https://companyname.atlassian.net/browse/JIRA-$current_branch_number)
+[$(getTitle)](https://globalradio.atlassian.net/browse/JIRA-$current_branch_number)
 ---
-### Description: \n
+<h3>Description:</h3> \n
 \n
-### What was changed: \n
+<h3>What was changed:</h3> \n
 \n
-### Screenshots/videos/gif:
+<h3>Screenshots/videos/gif:</h3>
+\n
+\n
+<details>
+  <summary>PR Tips</summary>
+
+  - How to build the template script: ./gradlew makepr
+  - The bash script name is: make_pr.sh
+  - The table example for copy paste:
+
+  | Before | After |
+  |------|------|
+  |Your image before|Your image after|
+
+</details>
 "
     echo -e "$description"
 }
@@ -54,6 +80,7 @@ function getPrDescription() {
 base_url="https://github.com"
 target_branch=$(getTargetBranch)
 current_branch="$(git rev-parse --abbrev-ref HEAD)"
+# Will change company_repo and company_project ro real values of your project
 path="/company_repo/company_project/compare/$target_branch...$current_branch"
 
 #query params
